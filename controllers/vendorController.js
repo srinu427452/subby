13ee247f7024d11c12daa50040199a4ddda62eb9
Srinu7652 <srinuy1199@gmail.com>
2025-05-06 17:18:23 +0530

    completed vendor login v-5

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -29,5 +29,19 @@ const vendorRegister=async(req,res)=>{
         res.status(500).json({error:"Internal server error"})
     }
 }
+const vendorLogin=async(req, res)=>{
+    const {email,password}=req.body;
+    try{
+        const vendor=await Vendor.findOne({email});
+        if(!vendor || !(await bcrypt.compare(password,vendor.password))){
+            return res.status(401).json({error:"Invalid email or password"});
+        }
 
-module.exports={vendorRegister}
+        res.status(200).json({success:"Login successful"})
+        console.log(email)
+    }catch(error){
+        console.error(error);
+    }
+}
+
+module.exports={vendorRegister, vendorLogin}
